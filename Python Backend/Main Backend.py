21e26848f6bdfbdc9d863c271b744e58d78a49cb
Number22313 <Number22313@github.com>
2025-05-08 09:24:25 +1200

    Added code comments and started CSS style

--- Python Backend/Main Backend.py
+++ Python Backend/Main Backend.py
@@ -4,10 +4,18 @@ from flask import Flask,render_template
 app = Flask(__name__)
 
 
+#make home.html the first page to load
 @app.route('/')
 def home():
-    return render_template("home.html")
+    return render_template('home.html')
 
 
+#enable debugging
 if __name__ == '__main__':
     app.run(debug = True)
+
+
+#database example code
+#@app.route('/page_1/<int:id>')
+#def page_1():
+#   return render_template('page_1.html')
