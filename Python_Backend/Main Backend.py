4b65f4f0fc5f3cb6308d9ef96d90b796e4903c3a
Number22313 <Number22313@github.com>
2025-08-06 14:28:14 +1200

    Added visual progress bars to go alongside percentages and CSS session #1, 4.5 maps / 18 filled in

--- Python_Backend/Main Backend.py
+++ Python_Backend/Main Backend.py
@@ -5,6 +5,7 @@ logging.getLogger('werkzeug').setLevel(logging.WARNING) #Supress 304 and termina
 
 app = Flask(__name__)
 
+
 def get_db(): #Database connection function
     db = getattr(g, '_database', None)
     if db is None:
@@ -32,26 +33,63 @@ def home():
     cur = db.cursor()
     rows = cur.execute('SELECT * FROM Distances').fetchall()
     TotalStars = sum(row['distance'] for row in rows)
+    Percent = ((TotalStars * 100)//5800000)
     TotalStars = format(TotalStars, ',')
     CountrysideStars = sum(row['distance'] for row in rows if row['map'] == 'Countryside')
+    Percent1 = ((CountrysideStars *100)//310000)
+    CountrysideStars = format(CountrysideStars, ',')
     ForestStars = sum(row['distance'] for row in rows if row['map'] == 'Forest')
+    Percent2 = ((ForestStars *100)//310000)
+    ForestStars = format(ForestStars, ',')
     CityStars = sum(row['distance'] for row in rows if row['map'] == 'City')
+    Percent3 = ((CityStars *100)//310000)
+    CityStars = format(CityStars, ',')
     MountainStars = sum(row['distance'] for row in rows if row['map'] == 'Mountain')
+    Percent4 = ((MountainStars *100)//310000)
+    MountainStars = format(MountainStars, ',')
     ReefStars = sum(row['distance'] for row in rows if row['map'] == 'Reef')
+    Percent5 = ((ReefStars *100)//310000)
+    ReefStars = format(ReefStars, ',')
     WinterStars = sum(row['distance'] for row in rows if row['map'] == 'Winter')
+    Percent6 = ((WinterStars *100)//310000)
+    WinterStars = format(WinterStars, ',')
     MinesStars = sum(row['distance'] for row in rows if row['map'] == 'Mines')
+    Percent7 = ((MinesStars *100)//310000)
+    MinesStars = format(MinesStars, ',')
     DesertvalleyStars = sum(row['distance'] for row in rows if row['map'] == 'Desert')
+    Percent8 = ((DesertvalleyStars *100)//310000)
+    DesertvalleyStars = format(DesertvalleyStars, ',')
     BeachStars = sum(row['distance'] for row in rows if row['map'] == 'Beach')
+    Percent9 = ((BeachStars *100)//310000)
+    BeachStars = format(BeachStars, ',')
     BogStars = sum(row['distance'] for row in rows if row['map'] == 'Bog')
+    Percent10 = ((BogStars *100)//310000)
+    BogStars = format(BogStars, ',')
     GlacierStars = sum(row['distance'] for row in rows if row['map'] == 'Glacier')
+    Percent11 = ((GlacierStars *100)//310000)
+    GlacierStars = format(GlacierStars, ',')
     PatchworkStars = sum(row['distance'] for row in rows if row['map'] == 'Patchwork')
+    Percent12 = ((PatchworkStars *100)//310000)
+    PatchworkStars = format(PatchworkStars, ',')
     SavannaStars = sum(row['distance'] for row in rows if row['map'] == 'Savanna')
+    Percent13 = ((SavannaStars *100)//310000)
+    SavannaStars = format(SavannaStars, ',')
     GloomvaleStars = sum(row['distance'] for row in rows if row['map'] == 'Gloomvale')
+    Percent14 = ((GloomvaleStars *100)//310000)
+    GloomvaleStars = format(GloomvaleStars, ',')
     OverspillStars = sum(row['distance'] for row in rows if row['map'] == 'Overspill')
-    CanyonarenaStars = sum(row['distance'] for row in rows if row['map'] == 'Arena')
+    Percent15 = ((OverspillStars *100)//310000)
+    OverspillStars = format(OverspillStars, ',')
+    CanyonarenaStars = sum(row['distance'] for row in rows if row['map'] == 'Canyonarena')
+    Percent16 = ((CanyonarenaStars *100)//310000)
+    CanyonarenaStars = format(CanyonarenaStars, ',')
     CuptownStars = sum(row['distance'] for row in rows if row['map'] == 'Cuptown')
+    Percent17 = ((CuptownStars *100)//310000)
+    CuptownStars = format(CuptownStars, ',')
     MoonStars = sum(row['distance'] for row in rows if row['map'] == 'Moon')
-    return render_template('home.html', rows=rows, TotalStars=TotalStars, 
+    Percent18 = ((MoonStars *100)//310000)
+    MoonStars = format(MoonStars, ',')
+    return render_template('home.html', rows=rows, TotalStars=TotalStars,
                            CountrysideStars=CountrysideStars,
                            ForestStars=ForestStars,
                            CityStars=CityStars,
@@ -69,7 +107,26 @@ def home():
                            OverspillStars=OverspillStars,
                            CanyonarenaStars=CanyonarenaStars,
                            CuptownStars=CuptownStars,
-                           MoonStars=MoonStars
+                           MoonStars=MoonStars,
+                           Percent=Percent,
+                           Percent1=Percent1,
+                           Percent2=Percent2,
+                           Percent3=Percent3,
+                           Percent4=Percent4,
+                           Percent5=Percent5,
+                           Percent6=Percent6,
+                           Percent7=Percent7,
+                           Percent8=Percent8,
+                           Percent9=Percent9,
+                           Percent10=Percent10,
+                           Percent11=Percent11,
+                           Percent12=Percent12,
+                           Percent13=Percent13,
+                           Percent14=Percent14,
+                           Percent15=Percent15,
+                           Percent16=Percent16,
+                           Percent17=Percent17,
+                           Percent18=Percent18
                            )
 
 
@@ -113,11 +170,11 @@ def Form10k():
     forms = c.execute('SELECT * FROM Distances').fetchall()
 
     #Convert data to dictionary
-    Rows = {}
+    Forms = {}
     for form in forms:
-        Rows[(form['map'], form['vehicle'])] = form['distance']
-    
-    return render_template('10k_Form.html', forms=forms, Rows=Rows)
+        Forms[(form['map'], form['vehicle'])] = form['distance']
+
+    return render_template('10k_Form.html', forms=forms, Forms=Forms)
 
 
 #Delete function for removing data
