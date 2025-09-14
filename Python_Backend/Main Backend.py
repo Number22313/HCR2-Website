57e57662da91d5409ebe2164fb79e23e4d906e9f
Number22313 <Number22313@github.com>
2025-09-14 13:12:14 +1200

    Changed the style for a more minimalist design and added a statistics section which still needs styling but is funcitonal

--- Python_Backend/Main Backend.py
+++ Python_Backend/Main Backend.py
@@ -29,67 +29,139 @@ def close_db(exception):
 #Main home page route with database table
 @app.route('/home', methods=['GET', 'POST'])
 def home():
+    return render_template('home.html')
+
+
+#10k form page route and POST debugging
+@app.route('/10k_Form.html', methods=['GET', 'POST'])
+def Form10k():
     db = get_db()
-    cur = db.cursor()
-    rows = cur.execute('SELECT * FROM Distances').fetchall()
+    c = db.cursor()
+    rows = c.execute('SELECT * FROM Distances').fetchall()
     TotalStars = sum(row['distance'] for row in rows)
+    TotalDistance = TotalStars // 1000
+    Total10ks = sum(row['distance'] // 10000 for row in rows)
+    TotalPercent = (( Total10ks * 100) // 558)
     Percent = ((TotalStars * 100)//5800000)
-    TotalStars = format(TotalStars, ',')
     CountrysideStars = sum(row['distance'] for row in rows if row['map'] == 'Countryside')
+    CountrysideStars = 310000
     Percent1 = ((CountrysideStars *100)//310000)
-    CountrysideStars = format(CountrysideStars, ',')
     ForestStars = sum(row['distance'] for row in rows if row['map'] == 'Forest')
     Percent2 = ((ForestStars *100)//310000)
-    ForestStars = format(ForestStars, ',')
     CityStars = sum(row['distance'] for row in rows if row['map'] == 'City')
     Percent3 = ((CityStars *100)//310000)
-    CityStars = format(CityStars, ',')
     MountainStars = sum(row['distance'] for row in rows if row['map'] == 'Mountain')
     Percent4 = ((MountainStars *100)//310000)
-    MountainStars = format(MountainStars, ',')
     ReefStars = sum(row['distance'] for row in rows if row['map'] == 'Reef')
     Percent5 = ((ReefStars *100)//310000)
-    ReefStars = format(ReefStars, ',')
     WinterStars = sum(row['distance'] for row in rows if row['map'] == 'Winter')
     Percent6 = ((WinterStars *100)//310000)
-    WinterStars = format(WinterStars, ',')
     MinesStars = sum(row['distance'] for row in rows if row['map'] == 'Mines')
     Percent7 = ((MinesStars *100)//310000)
-    MinesStars = format(MinesStars, ',')
     DesertvalleyStars = sum(row['distance'] for row in rows if row['map'] == 'Desert')
     Percent8 = ((DesertvalleyStars *100)//310000)
-    DesertvalleyStars = format(DesertvalleyStars, ',')
     BeachStars = sum(row['distance'] for row in rows if row['map'] == 'Beach')
     Percent9 = ((BeachStars *100)//310000)
-    BeachStars = format(BeachStars, ',')
     BogStars = sum(row['distance'] for row in rows if row['map'] == 'Bog')
     Percent10 = ((BogStars *100)//310000)
-    BogStars = format(BogStars, ',')
     GlacierStars = sum(row['distance'] for row in rows if row['map'] == 'Glacier')
     Percent11 = ((GlacierStars *100)//310000)
-    GlacierStars = format(GlacierStars, ',')
     PatchworkStars = sum(row['distance'] for row in rows if row['map'] == 'Patchwork')
     Percent12 = ((PatchworkStars *100)//310000)
-    PatchworkStars = format(PatchworkStars, ',')
     SavannaStars = sum(row['distance'] for row in rows if row['map'] == 'Savanna')
     Percent13 = ((SavannaStars *100)//310000)
-    SavannaStars = format(SavannaStars, ',')
     GloomvaleStars = sum(row['distance'] for row in rows if row['map'] == 'Gloomvale')
     Percent14 = ((GloomvaleStars *100)//310000)
-    GloomvaleStars = format(GloomvaleStars, ',')
     OverspillStars = sum(row['distance'] for row in rows if row['map'] == 'Overspill')
     Percent15 = ((OverspillStars *100)//310000)
-    OverspillStars = format(OverspillStars, ',')
     CanyonarenaStars = sum(row['distance'] for row in rows if row['map'] == 'Canyonarena')
     Percent16 = ((CanyonarenaStars *100)//310000)
-    CanyonarenaStars = format(CanyonarenaStars, ',')
     CuptownStars = sum(row['distance'] for row in rows if row['map'] == 'Cuptown')
     Percent17 = ((CuptownStars *100)//310000)
-    CuptownStars = format(CuptownStars, ',')
     MoonStars = sum(row['distance'] for row in rows if row['map'] == 'Moon')
     Percent18 = ((MoonStars *100)//310000)
+    Map_Stars = [CountrysideStars, ForestStars, CityStars, 
+                 MountainStars, ReefStars, WinterStars, 
+                 MinesStars, DesertvalleyStars, BeachStars,
+                 BogStars, GlacierStars, PatchworkStars,
+                 SavannaStars, GloomvaleStars, OverspillStars,
+                 CanyonarenaStars, CuptownStars, MoonStars]
+    MaxMaps = sum(1 for i in Map_Stars if i == 310000)
+    TotalStars = format(TotalStars, ',')
+    CountrysideStars = format(CountrysideStars, ',')
+    ForestStars = format(ForestStars, ',')
+    CityStars = format(CityStars, ',')
+    MountainStars = format(MountainStars, ',')
+    ReefStars = format(ReefStars, ',')
+    WinterStars = format(WinterStars, ',')
+    MinesStars = format(MinesStars, ',')
+    DesertvalleyStars = format(DesertvalleyStars, ',')
+    BeachStars = format(BeachStars, ',')
+    BogStars = format(BogStars, ',')
+    GlacierStars = format(GlacierStars, ',')
+    PatchworkStars = format(PatchworkStars, ',')
+    SavannaStars = format(SavannaStars, ',')
+    GloomvaleStars = format(GloomvaleStars, ',')
+    OverspillStars = format(OverspillStars, ',')
+    CanyonarenaStars = format(CanyonarenaStars, ',')
+    CuptownStars = format(CuptownStars, ',')
     MoonStars = format(MoonStars, ',')
-    return render_template('home.html', rows=rows, TotalStars=TotalStars,
+
+    if request.method == 'POST':
+        Distance = request.form.get('Distance').strip()
+        Map = request.form.get('Map')
+        Vehicle = request.form.get('Vehicle')
+        print("Distance:", Distance)
+        print("Map:", Map)
+        print("Vehicle:", Vehicle)
+        allowed_maps = ["Countryside", "Forest", "City", "Mountain", "Reef", "Winter", "Mines",
+        "Desert Valley", "Beach", "Bog", "Glacier", "Patchwork", "Savanna",
+        "Gloomvale", "Overspill", "Canyon Arena", "Cuptown", "Moon"
+        ]
+
+        allowed_vehicles = [
+        "Jeep", "Scooter", "Bus", "Mk2", "Tractor", "Motocross", "Dune Buggy",
+        "Sports Car", "Monster Truck", "Rotator", "Super Diesel", "Chopper",
+        "Tank", "Lowrider", "Snowmobile", "Monowheel", "Beast", "RallyCar",
+        "Formula", "Muscle Car", "Racing Truck", "Hotrod", "CC-EV",
+        "Superbike", "Supercar", "Moonlander", "Rock Bouncer", "Hoverbike",
+        "Raider", "Glider", "Bolt"
+        ]
+
+        if Distance.isdigit() and Map in allowed_maps and Vehicle in allowed_vehicles:
+            Distance = int(Distance)
+            if 0 < Distance < 10001:
+                try: #Insert form data into db
+                    c.execute(
+                        'INSERT INTO Distances (map, vehicle, distance) VALUES (?,?,?)',
+                        (Map, Vehicle, Distance))
+                    print("Inserted")
+                    db.commit()
+                except sqlite3.IntegrityError: #Update db for duplicate data
+                    c.execute(
+                        'UPDATE Distances SET distance = ? WHERE map = ? AND vehicle = ?',
+                        (Distance, Map, Vehicle))
+                    print("Updated")
+                    db.commit()
+                print("POSTED")
+            else:
+                print("Distance not < 10000 or > 0")
+        else:
+            print("Not elegible for insertion")
+
+        return redirect(url_for('Form10k')) #Prevent form resubmition when reloading page
+
+    forms = c.execute('SELECT * FROM Distances').fetchall()
+
+    #Convert data to dictionary
+    Forms = {}
+    for form in forms:
+        Forms[(form['map'], form['vehicle'])] = form['distance']
+
+    return render_template('10k_Form.html', forms=forms,
+                           Forms=Forms,
+                           rows=rows, 
+                           TotalStars=TotalStars,
                            CountrysideStars=CountrysideStars,
                            ForestStars=ForestStars,
                            CityStars=CityStars,
@@ -126,55 +198,12 @@ def home():
                            Percent15=Percent15,
                            Percent16=Percent16,
                            Percent17=Percent17,
-                           Percent18=Percent18
+                           Percent18=Percent18,
+                           MaxMaps=MaxMaps,
+                           TotalDistance=TotalDistance,
+                           Total10ks=Total10ks,
+                           TotalPercent=TotalPercent
                            )
-
-
-#10k form page route and POST debugging
-@app.route('/10k_Form.html', methods=['GET', 'POST'])
-def Form10k():
-    print(request.method)
-    db = get_db()
-    c = db.cursor()
-
-    if request.method == 'POST':
-        Distance = request.form.get('Distance').strip()
-        Map = request.form.get('Map')
-        Vehicle = request.form.get('Vehicle')
-        print("Distance:", Distance)
-        print("Map:", Map)
-        print("Vehicle:", Vehicle)
-        if Distance.isdigit():
-            Distance = int(Distance)
-            if 0 < Distance < 10001:
-                try: #Insert form data into db
-                    c.execute(
-                        'INSERT INTO Distances (map, vehicle, distance) VALUES (?,?,?)',
-                        (Map, Vehicle, Distance))
-                    print("Inserted")
-                    db.commit()
-                except sqlite3.IntegrityError: #Update db for duplicate data
-                    c.execute(
-                        'UPDATE Distances SET distance = ? WHERE map = ? AND vehicle = ?',
-                        (Distance, Map, Vehicle))
-                    print("Updated")
-                    db.commit()
-                print("POSTED")
-            else:
-                print("Not < 10000 or > 0")
-        else:
-            print("Not numbers")
-
-        return redirect(url_for('Form10k')) #Prevent form resubmition when reloading page
-
-    forms = c.execute('SELECT * FROM Distances').fetchall()
-
-    #Convert data to dictionary
-    Forms = {}
-    for form in forms:
-        Forms[(form['map'], form['vehicle'])] = form['distance']
-
-    return render_template('10k_Form.html', forms=forms, Forms=Forms)
 
 
 #Delete function for removing data
